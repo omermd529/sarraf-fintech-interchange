1869833312bba8e16f9bb07ab5baa3ccef17140b
Omer Mohammed <omermd529@gmail.com>
2026-02-15 01:01:26 +0530

    add terraform/environments/dev/provider.tf add terraform/environments/prod/provider.tf

---
+++ terraform/environments/dev/provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "gcs" {
+    bucket = "omerops-sarraf-dev-tfstate-me-central1"
+    prefix = "terraform/dev/state"
+  }
+}
+
+provider "google" {
+  project     = "omerops-sarraf-dev"
+  region      = "me-central1" #Deployed in Doha region due to restrictions in Saudi Arabia Dammam region
+}
---
+++ terraform/environments/prod/provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "gcs" {
+    bucket = "omerops-sarraf-prod-tfstate-me-central1"
+    prefix = "terraform/prod/state"
+  }
+}
+
+provider "google" {
+  project     = "omerops-sarraf-prod"
+  region      = "me-central1"
+}
