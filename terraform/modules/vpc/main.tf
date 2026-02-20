d99e37f05c391574b14b7a6fc4ffac0bd3d8bc3c
Omer Mohammed <omermd529@gmail.com>
2026-02-20 12:13:22 +0530

    adding cloud router and cloud NAT to the VPC module.

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -37,3 +37,28 @@ resource "google_compute_firewall" "allow_iap_ssh" {
 
   source_ranges = ["35.235.240.0/20"]
 }
+
+resource "google_compute_router" "router" {
+  name    = "sarraf-router-${var.env}"
+  region  = var.region
+  network = google_compute_network.vpc.id
+  project = var.project_id
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "sarraf-nat-${var.env}"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  project                            = var.project_id
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.subnet.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
