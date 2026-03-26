c8758460ce8a84f41c30dbce750dc5ed5db1c5ed
Omer Mohammed <omermd529@gmail.com>
2026-03-27 01:54:00 +0530

    added a new outputs file

---
+++ terraform/environments/dev/outputs.tf
@@ -0,0 +1,10 @@
+output "database_ip" {
+  value       = module.database.db_private_ip
+  description = "The private IP address of the SQL instance for the backend"
+}
+
+# Optional: Add these for debugging
+output "database_instance_name" {
+  value = module.database.db_instance_name
+  description = "The name of the SQL instance for the backend"
+}
--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -6,12 +6,25 @@ resource "google_container_cluster" "primary" {
   name     = "sarraf-cluster-${var.env}"
   location = var.region
 
+  # Autopilot is the right choice for Doha (me-central1) efficiency
   enable_autopilot = true
+
+  # Fixes the "Error 400" by explicitly maintaining this state
+  secret_manager_config {
+    enabled = true
+  }
+
+  # REQUIRED: This links your K8s Service Accounts to Google IAM
+  workload_identity_config {
+    workload_pool = "${data.google_project.project.project_id}.svc.id.goog"
+  }
 
   network    = var.network_id
   subnetwork = var.subnet_id
 
+  # Labels are for metadata/billing filters
   resource_labels = {
+    env                = var.env
     provisioning_model = var.is_spot ? "spot" : "standard"
   }
 
@@ -20,6 +33,7 @@ resource "google_container_cluster" "primary" {
     services_secondary_range_name = "k8s-service-range"
   }
 
+  # Temporary allow-all for debugging; remember to tighten this later!
   master_authorized_networks_config {
     cidr_blocks {
       cidr_block   = "0.0.0.0/0"
@@ -30,4 +44,7 @@ resource "google_container_cluster" "primary" {
   release_channel {
     channel = "REGULAR"
   }
+
+  # Prevents accidental deletion of the cluster during 'terraform destroy'
+  deletion_protection = false 
 }
